bcf95d7d62be619cea40eae6424fde8670dd7f8d
Pantazis <stathispantazis7@hotmail.com>
2019-02-19 15:25:44 +0200

    DomainModels Interfaces

---
+++ DomainModels.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace DomainModels.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void WinnerReturns()
+        {
+            //Class1 c = new Class1();
+            //string x = "takis";
+            //string y = "takise";
+            
+            //int expected = 14;
+            //Assert.Throws<NotImplementedException>(() => c.Winner(x,y));
+        }
+    }
+}
